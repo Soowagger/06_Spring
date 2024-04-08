7c6cbf6a8a3e34527a24ded0b70e4e9b3a608efd
Soowagger <soowagger@naver.com>
2024-04-08 17:40:26 +0900

    Ajax

--- todoList-boot/src/main/java/edu/kh/todo/model/service/TodoServiceImpl.java
+++ todoList-boot/src/main/java/edu/kh/todo/model/service/TodoServiceImpl.java
@@ -101,5 +101,28 @@ public class TodoServiceImpl implements TodoService {
 		
 		return mapper.todoDelete(todoNo);
 	}
+
+
+	// 전체 할 일 개수 조회
+	@Override
+	public int getTotalCount() {
+		
+		return mapper.getTotalCount();
+	}
+
+	
+	// 완료된 할 일 개수 조회
+	@Override
+	public int getCompleteCount() {
+		
+		return mapper.getCompleteCount();
+	}
+
+
+	@Override
+	public List<Todo> selectList() {
+		
+		return mapper.selectAll();
+	}
 	
 }
