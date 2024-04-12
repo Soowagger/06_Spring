c773684c2a9b167aebec959af4cea68239dca0dc
Soowagger <soowagger@naver.com>
2024-04-12 16:48:28 +0900

    boardProject 회원가입 끝

--- boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
+++ boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
@@ -19,5 +19,19 @@ public interface MemberMapper {
 	 * @return count
 	 */
 	int checkEmail(String memberEmail);
+
+
+	/** 닉네임 중복검사
+	 * @param memberNickname
+	 * @return count
+	 */
+	int checkNickname(String memberNickname);
+
+
+	/** 회원가입 SQL 실행
+	 * @param inputMember
+	 * @return result
+	 */
+	int signup(Member inputMember);
 	
 }
--- boardProject-boot/src/main/java/edu/kh/project/member/model/service/MemberService.java
+++ boardProject-boot/src/main/java/edu/kh/project/member/model/service/MemberService.java
@@ -10,6 +10,23 @@ public interface MemberService {
 	 */
 	Member login(Member inputMember);
 
+	/** 이메일 중복 검사
+	 * @param memberEmail
+	 * @return count
+	 */
 	int checkEmail(String memberEmail);
+
+	/** 닉네임 중복 검사 서비스
+	 * @param memberNickname
+	 * @return count
+	 */
+	int checkNickname(String memberNickname);
+
+	/** 회원 가입 서비스
+	 * @param inputMember
+	 * @param memberAddress
+	 * @return result
+	 */
+	int signup(Member inputMember, String[] memberAddress);
 
 }
